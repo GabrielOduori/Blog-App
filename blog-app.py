67d6a1dc836c4f238e62887efcd560b453011a4f
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-04-15 00:52:17 +0300

    Main app
    Created code for the main app entry point.

--- blog-app.py
+++ blog-app.py
@@ -4,7 +4,7 @@ from models import User,Role
 from flask_migrate import Migrate
 
 
-app  = create_app(os.getenv('FLASK_CONFIG') or default)
+app  = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
 
