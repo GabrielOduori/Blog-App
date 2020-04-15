8479582e0406ba70afa02d3e1da05e974276b388
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-04-16 00:25:59 +0300

    Tests
    Added a custom command to to run unit tests.

--- blog-app.py
+++ blog-app.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app, db
-from models import User,Role
+from app.modelsmodels import User,Role
 from flask_migrate import Migrate
 
 
@@ -12,3 +12,13 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_ontext:
     return dict(db=db,User=User,Role=Role)
+
+
+@app.cli.command()
+def test():
+    """
+    Run the unit tests.
+    """
+    import unittest 
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestResult(verbosity=2).run(tests)
