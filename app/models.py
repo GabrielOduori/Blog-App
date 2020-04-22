225d1e47f5c1454bec6f3c00f756f0d8f92b82ed
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-04-22 23:13:01 +0300

    Login
    Added a login module for the authentication.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 class Role(db.Model):
@@ -12,12 +13,14 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
 
 
     def __repr__(self):
@@ -33,6 +36,13 @@ class User(db.Model):
 
     def verify_password(self,password):
         return check_password_hash(self.password_hash, password)
+
+
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(user_id)
 
 
 
